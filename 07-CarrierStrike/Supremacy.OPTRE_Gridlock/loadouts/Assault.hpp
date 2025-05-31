34b161501f7db97793576a786fc7966dd39c70a1
ramsey-droid <thunt501stramsey@gmail.com>
2025-05-31 23:06:52 +0100

    carrier

---
+++ 07-CarrierStrike/Supremacy.OPTRE_Gridlock/loadouts/Assault.hpp
@@ -0,0 +1,324 @@
+class Tier1_West_Assault
+{
+    displayName = "Rifleman";
+    icon = "";
+    role = QGVAR(Tier1);
+    show = "true";
+    uniformClass = "JLTS_CloneArmor";
+    backpack = "";
+    weapons[] = {
+        "3AS_DC15A_F",
+        "3AS_DC17S_F",
+        "Throw",
+        "Put"
+    };
+    magazines[] = {
+        "Chemlight_blue",
+        "SmokeShellBlue",
+        "SmokeShellBlue",
+        "mti_Mag_ThermalDet",
+        "mti_Mag_ThermalDet",
+        "3AS_45Rnd_EC50_Mag",
+        "3AS_45Rnd_EC50_Mag",
+        "3AS_45Rnd_EC50_Mag",
+        "3AS_45Rnd_EC50_Mag",
+        "3AS_45Rnd_EC50_Mag",
+        "3AS_45Rnd_EC50_Mag",
+        "3AS_16Rnd_EC20_Mag",
+        "3AS_16Rnd_EC20_Mag"
+    };
+    items[] = {"FirstAidKit"};
+    linkedItems[] = {
+        // vest
+        "JLTS_CloneVestHolster",
+
+        // hat
+        "SEA_Helmet_P1_Base",
+
+        // equipment
+        "ItemMap",
+        "ItemCompass",
+        "ItemWatch",
+        "ItemRadio",
+
+        // optics
+        ""
+
+        // suppress
+    };
+};
+
+class Tier2_West_Assault
+{
+    displayName = "Grenadier";
+    icon = "";
+    role = QGVAR(Tier2);
+    show = "true";
+    uniformClass = "JLTS_CloneArmor";
+    backpack = "JLTS_Clone_backpack";
+    weapons[] = {
+        "3AS_DC15A_GL",
+        "3AS_DC17S_F",
+        "Throw",
+        "Put"
+    };
+    magazines[] = {
+        "Chemlight_blue",
+        "SmokeShellBlue",
+        "SmokeShellBlue",
+        "mti_Mag_ThermalDet",
+        "mti_Mag_ThermalDet",
+        "1Rnd_HE_Grenade_shell",
+        "1Rnd_HE_Grenade_shell",
+        "1Rnd_HE_Grenade_shell",
+        "1Rnd_HE_Grenade_shell",
+        "1Rnd_HE_Grenade_shell",
+        "1Rnd_HE_Grenade_shell",
+        "3AS_45Rnd_EC50_Mag",
+        "3AS_45Rnd_EC50_Mag",
+        "3AS_45Rnd_EC50_Mag",
+        "3AS_45Rnd_EC50_Mag",
+        "3AS_45Rnd_EC50_Mag",
+        "3AS_45Rnd_EC50_Mag",
+        "3AS_16Rnd_EC20_Mag",
+        "3AS_16Rnd_EC20_Mag"
+    };
+    items[] = {"FirstAidKit"};
+    linkedItems[] = {
+        // vest
+        "JLTS_CloneVestKama",
+
+        // hat
+        "SEA_Helmet_SpecOps_LR_Base",
+
+        // equipment
+        "ItemMap",
+        "ItemCompass",
+        "ItemWatch",
+        "ItemRadio",
+
+        // optics
+        "3AS_optic_DC15LE_F"
+
+        // suppress
+    };
+};
+
+class Tier3_West_Assault
+{
+    displayName = "Assault";
+    icon = "";
+    role = QGVAR(Tier3);
+    show = "true";
+    uniformClass = "JLTS_CloneArmor";
+    backpack = "JLTS_Clone_backpack";
+    weapons[] = {
+        "arifle_MX_GL_F",
+        "3AS_DC17S_F",
+        "Throw",
+        "Put"
+    };
+    magazines[] = {
+        "Chemlight_blue",
+        "SmokeShellBlue",
+        "SmokeShellBlue",
+        "mti_Mag_ThermalDet",
+        "mti_Mag_ThermalDet",
+        "1Rnd_HE_Grenade_shell",
+        "1Rnd_HE_Grenade_shell",
+        "1Rnd_HE_Grenade_shell",
+        "1Rnd_HE_Grenade_shell",
+        "1Rnd_HE_Grenade_shell",
+        "1Rnd_HE_Grenade_shell",
+        "3AS_45Rnd_EC50_Mag",
+        "3AS_45Rnd_EC50_Mag",
+        "3AS_45Rnd_EC50_Mag",
+        "3AS_45Rnd_EC50_Mag",
+        "3AS_45Rnd_EC50_Mag",
+        "3AS_45Rnd_EC50_Mag",
+        "3AS_16Rnd_EC20_Mag",
+        "3AS_16Rnd_EC20_Mag"
+    };
+    items[] = {"FirstAidKit"};
+    linkedItems[] = {
+        // vest
+        "V_PlateCarrierSpec_rgr",
+
+        // hat
+        "H_HelmetSpecB",
+
+        // equipment
+        "ItemMap",
+        "ItemCompass",
+        "ItemWatch",
+        "ItemRadio",
+
+        // optics
+        "optic_Arco_blk_F"
+
+        // suppress
+    };
+};
+
+//////////////////////////////////////////////////////////////////////////////////////////////////
+class Tier1_East_Assault
+{
+    displayName = "Rifleman";
+    icon = "";
+    role = QGVAR(Tier1);
+    show = "true";
+    uniformClass = "JLTS_DroidB1";
+    backpack = "JLTS_B1_backpack";
+    weapons[] = {
+        "3AS_E5_F",
+        "3AS_A180_F",
+        "Throw",
+        "Put"
+    };
+    magazines[] = {
+        "Chemlight_red",
+        "SmokeShellRed",
+        "SmokeShellRed",
+        "mti_Mag_ThermalDet",
+        "mti_Mag_ThermalDet",
+        "3AS_60Rnd_EM50_RedPlasma",
+        "3AS_60Rnd_EM50_RedPlasma",
+        "3AS_60Rnd_EM50_RedPlasma",
+        "3AS_60Rnd_EM50_RedPlasma",
+        "3AS_60Rnd_EM50_RedPlasma",
+        "3AS_60Rnd_EM50_RedPlasma",
+        "3AS_18Rnd_EM20_Mag",
+        "3AS_18Rnd_EM20_Mag"
+    };
+    items[] = {"FirstAidKit"};
+    linkedItems[] = {
+        // vest
+        "",
+
+        // hat
+        "",
+
+        // equipment
+        "ItemMap",
+        "ItemCompass",
+        "ItemWatch",
+        "ItemRadio",
+
+        // optics
+        "optic_Aco"
+
+        // suppress
+    };
+};
+
+class Tier2_East_Assault
+{
+    displayName = "Grenadier";
+    icon = "";
+    role = QGVAR(Tier2);
+    show = "true";
+    uniformClass = "mti_factions_cis_B1Support";
+    backpack = "JLTS_B1_backpack";
+    weapons[] = {
+        "3AS_DC15C_GL",
+        "3AS_A180_F",
+        "Throw",
+        "Put"
+    };
+    magazines[] = {
+        "Chemlight_red",
+        "SmokeShellRed",
+        "SmokeShellRed",
+        "mti_Mag_ThermalDet",
+        "mti_Mag_ThermalDet",
+        "1Rnd_HE_Grenade_shell",
+        "1Rnd_HE_Grenade_shell",
+        "1Rnd_HE_Grenade_shell",
+        "1Rnd_HE_Grenade_shell",
+        "1Rnd_HE_Grenade_shell",
+        "1Rnd_HE_Grenade_shell",
+        "3AS_40Rnd_EM40_Mag",
+        "3AS_40Rnd_EM40_Mag",
+        "3AS_40Rnd_EM40_Mag",
+        "3AS_40Rnd_EM40_Mag",
+        "3AS_40Rnd_EM40_Mag",
+        "3AS_40Rnd_EM40_Mag",
+        "3AS_16Rnd_EC20_Mag",
+        "3AS_16Rnd_EC20_Mag"
+    };
+    items[] = {"FirstAidKit"};
+    linkedItems[] = {
+        // vest
+        "",
+
+        // hat
+        "",
+
+        // equipment
+        "ItemMap",
+        "ItemCompass",
+        "ItemWatch",
+        "ItemRadio",
+
+        // optics
+        "3AS_optic_DC15C_F"
+
+        // suppress
+    };
+};
+
+class Tier3_East_Assault
+{
+    displayName = "Assault";
+    icon = "";
+    role = QGVAR(Tier3);
+    show = "true";
+    uniformClass = "mti_factions_cis_B1Heavy";
+    backpack = "mti_factions_cis_pack_B1_Heavy";
+    weapons[] = {
+        "E5C_F",
+        "3AS_A180_F",
+        "Throw",
+        "Put"
+    };
+    magazines[] = {
+        "Chemlight_red",
+        "SmokeShellRed",
+        "SmokeShellRed",
+        "mti_Mag_ThermalDet",
+        "mti_Mag_ThermalDet",
+        "1Rnd_HE_Grenade_shell",
+        "1Rnd_HE_Grenade_shell",
+        "1Rnd_HE_Grenade_shell",
+        "1Rnd_HE_Grenade_shell",
+        "1Rnd_HE_Grenade_shell",
+        "1Rnd_HE_Grenade_shell",
+        "3AS_130Rnd_EM50_RedPlasma",
+        "3AS_130Rnd_EM50_RedPlasma",
+        "3AS_130Rnd_EM50_RedPlasma",
+        "3AS_130Rnd_EM50_RedPlasma",
+        "3AS_130Rnd_EM50_RedPlasma",
+        "3AS_130Rnd_EM50_RedPlasma",
+        "3AS_16Rnd_EC20_Mag",
+        "3AS_16Rnd_EC20_Mag"
+    };
+    items[] = {"FirstAidKit"};
+    linkedItems[] = {
+        // vest
+        "",
+
+        // hat
+        "",
+
+        // equipment
+        "ItemMap",
+        "ItemCompass",
+        "ItemWatch",
+        "ItemRadio",
+
+        // optics
+        "optic_Aco"
+
+        // suppress
+    };
+};
